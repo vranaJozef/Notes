0580df8c3d1763c76bc9f01cf4a5f5d256e2ec0a
Jozef Vrana <vranaJozef7652@gmail.com>
2019-03-10 12:17:58 +0100

    Initial commit

---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by Jozef Vrana on 10/03/2019.
+//  Copyright © 2019 Jozef Vrana. All rights reserved.
+//
+
+import XCTest
+@testable import Notes
+
+class NotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
